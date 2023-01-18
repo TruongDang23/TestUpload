7a85b578f09cd86da6d6244a431c4ba9c97280c7
TruongDang23 <quantruong050123@gmail.com>
2023-01-18 22:57:33 +0700

    rebuilt Welcome

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -11,35 +11,7 @@ function Welcome(props)
 {
     return (
         <View style={{flex:1}}>
-            <ImageBackground 
-             source={images.background}
-             resizeMode='cover'
-             style={{
-                flex:1,
-             }}
-            >
-                <View style={{
-                    flex:1,
-                    justifyContent:'center',
-                    alignItems:'center',
-                    //backgroundColor:'yellow',
-                }}>
-                    <Image 
-                        source={images.logo}
-                        style={{
-                            width:150,//width/height ~= 31/40
-                            height:194
-                        }}
-                    />
-                </View>
-                <View style={{
-                    flex:1,
-                    backgroundColor:'purple',
-                }}>
-                    <UIButton/>
-                </View>
-                <Text>Hello World</Text>
-            </ImageBackground>
+            <Text>Hello World</Text>
         </View>
     )
 }
